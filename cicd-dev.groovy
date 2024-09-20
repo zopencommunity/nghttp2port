d5783e9eca16bff1daa46a6711b31529e1e08443
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:20 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/nghttp2port.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/nghttp2port.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/nghttp2port.git'), string(name: 'PORT_DESCRIPTION', value: 'This is an implementation of the Hypertext Transfer Protocol version 2 in C.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/nghttp2port.git'), string(name: 'PORT_DESCRIPTION', value: 'This is an implementation of the Hypertext Transfer Protocol version 2 in C.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
